53d5ce79b849f8e8fc59905079f52733b678c631
KolesnichenkoAG <kag.1981@mail.ru>
2023-01-31 23:06:32 +0600

    дз урок 3

---
+++ winter-market-master/winter-market/src/main/java/ru/geekbrains/aspect/FirstLogAspect.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class FirstLogAspect {
+
+    @Before("execution (* ru.geekbrains.*.*.*(String))")
+    public void beforeFirstService(JoinPoint jp) {
+        Class<?> beanClass = jp.getTarget().getClass();
+        String methodName = jp.getSignature().getName();
+        String arg = (String) jp.getArgs()[5];
+        log.debug("Был вызван метод {}#{} с аргументом {}", beanClass.getName(), methodName, arg);
+    }
+}
