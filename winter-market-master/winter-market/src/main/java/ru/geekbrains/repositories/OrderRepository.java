f0752e785d8afd64f33c6d9577408b1ba54d61c8
KolesnichenkoAG <kag.1981@mail.ru>
2023-02-08 00:29:37 +0600

    дз урок 4

--- winter-market-master/winter-market/src/main/java/ru/geekbrains/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.geekbrains.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.geekbrains.entities.Order;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
